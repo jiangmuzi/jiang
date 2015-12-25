8f2f94722d458d2468437f90501c4cec9440c1a9
jiangmuzi <sisome@qq.com>
2015-12-25 10:27:25 +0800

    fix a bug

--- 404.php
+++ 404.php
@@ -1,16 +1,17 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
-
-    <div class="col-mb-12 col-tb-8 col-tb-offset-2">
-
-        <div class="error-page">
-            <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
+<section class="blog-banner page">
+	<div class="blog-banner-img" style="background-image:url('<?php $this->options->themeUrl('img/banner.jpg');?>');"></div>
+	<h2>页面没找到</h2>
+</section>
+<article class="post">
+	<div class="error-page">
+            <h2>404 - <?php _e('页面没找到'); ?></h2>
             <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
             <form method="post">
                 <p><input type="text" name="s" class="text" autofocus /></p>
-                <p><button type="submit" class="submit"><?php _e('搜索'); ?></button></p>
+                <p><button type="submit" class="btn"><?php _e('搜索'); ?></button></p>
             </form>
         </div>
-
-    </div><!-- end #content-->
-	<?php $this->need('footer.php'); ?>
+</article>
+<?php $this->need('footer.php'); ?>
